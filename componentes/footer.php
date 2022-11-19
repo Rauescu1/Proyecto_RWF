8c36b50de6eca139f7d0cc4ddde9e3804a9ff8f8
Raul <rauescu@gmail.com>
2022-11-19 13:16:10 +0100

    Version0.0
    Empezando con el proyecto, la estructuras principales respecto al header y al footer

---
+++ componentes/footer.php
@@ -0,0 +1,8 @@
+<div>
+    <img src="./src/head/rwflogo.png" width = 110px>
+    <a href="">Politica de Privacidad - Politica de Cookies</a>
+    <br>
+    <a href="">18213 Alfacar, Granada Spain</a>
+    <br>
+    <p>Racing Welds Factory</p>
+</div>
---
+++ componentes/header.php
@@ -0,0 +1,10 @@
+
+<div class = "header">
+    <img src="./src/head/rwflogo.png" width = 110px>
+    <a href="">PROYECTOS</a>
+    &nbsp;&nbsp;&nbsp;
+    <a href="">PRODUCTOS</a>
+    &nbsp;&nbsp;&nbsp;
+    <a href="">SERVICIO TECNICO</a>
+
+</div>
