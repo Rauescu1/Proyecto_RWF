6e3546ba8433de3c9950cca4444b7a4bd1ceaf5b
Raul <rauescu@gmail.com>
2022-11-27 23:16:57 +0100

    Version0.2.1
    Actualizacion de los cuadrados.

--- componentes/subdestacados.php
+++ componentes/subdestacados.php
@@ -3,22 +3,21 @@
             <h2>BMW 330d</h2>
             <img src="./src/fav/1/bmw1.jpg">
             <p id="descripcion">L&iacute;nea completa con doble salida para difusor ///M manteniendo el plástico inferior</p>
-            <div class = "mas">
-                <p></p>
-            </div>
-
+            <a href="" class = "mas">MAS ➤</a>
         </div>
 
         <div class="favoritos">
-        <h2>BMW 323ti</h2>
-        <img src="./src/fav/2/2.jpg">
-        <p id="descripcion">Tramo final con doble cola en 3'</p>
+            <h2>BMW 323ti</h2>
+            <img src="./src/fav/2/2.jpg">
+            <p id="descripcion">Tramo final con doble cola en 3'</p>    
+            <a href="" class = "mas">MAS ➤</a>
         </div>
 
         <div class="favoritos">
-        <h2>Nissan 350Z</h2>
-        <img src="./src/fav/3/3.jpg">
-        <p id="descripcion">Se le realizo una Y nueva, un tramo final y un cambio de todas las bridas de serie por unas nuevas V-Band</p>
+            <h2>Nissan 350Z</h2>
+            <img src="./src/fav/3/3.jpg">
+            <p id="descripcion">Se le realizo una Y nueva, un tramo final y un cambio de todas las bridas de serie por unas nuevas V-Band</p>
+            <a href="" class="mas">MAS ➤</a>
         </div>
 
 
